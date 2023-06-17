9bd117c41991f954af9eaf51fb44663333ea6e19
ErnestoRdS <egeregogbu@gmail.com>
2023-06-16 19:37:32 -0600

    inputver.py: se modificó la función principal de verificación para que salga y envíe un bonito mensaje

--- funcs/inputver.py
+++ funcs/inputver.py
@@ -1,5 +1,8 @@
 # Función para verificar que el input esté bien, de lo contrario, volver a pedirlo
 def verifyInput(input):
+    if int(input) == -1:
+        print("Bye-bye, cuídate")
+        exit()
     if not input.isnumeric():
         print(f"Introduce un valor correcto, por favor ")
         return input, False
@@ -11,8 +14,6 @@ def verifyInput(input):
 def scanf(input):
     out, flag = verifyInput(input)
 
-    if int(out) == -1:
-        exit()
     if not flag:
         print(out)
         return scanf(input)
