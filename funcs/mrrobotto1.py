a27b58bcc9be9a30b8ef6299ec6d1394f8ef79e5
ErnestoRdS <egeregogbu@gmail.com>
2023-06-19 12:38:22 -0600

    mrrobotto1.py: separar la función generadora de contraseñas

---
+++ funcs/mrrobotto1.py
@@ -0,0 +1,23 @@
+# Importar string para la generación de letras al azar
+import string as str
+# Importar random
+import random as rnd
+
+# Para generar las contraseñas
+def genos(lenght, samples):
+
+    # Tomar los caracteres para la contraseña del siguiente conjuntod de caracteres
+    chars = str.ascii_letters + str.digits + str.punctuation
+    # Para guardar las contraseñas generadas
+    contras = []
+
+    for i in range(samples):
+        temp = ""
+        # Iterar por el número de caracteres de longitud
+        for char in range(lenght):
+            temp += chars[rnd.choice(chars)]
+        # Añadir la posible constraseña recién generada a la lista que regresaremos
+        contras.append(temp)
+    
+    # Regresar los prospectos
+    return contras
